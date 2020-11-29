d0ff4f580bfc0a8a4f50fea80e8c5b99cf421065
dokutan <54861821+dokutan@users.noreply.github.com>
2020-11-29 12:29:47 +0100

    Update Lua examples

--- create-config.lua
+++ create-config.lua
@@ -12,11 +12,11 @@ settings = { vid = "046a", pid = "0011", use_bus_device = false, bus = 1, device
 -- These settings are for the libevdev backend
 -- settings = { eventfile = "/dev/input/by-id/u.sb-046a_0011-event-kbd", grab = true, numbers = false }
 
-print( "-- Copy the following output into the input_handler function" )
+print( "-- Copy the following output into the input handler function" )
 print( "-- Press any keys (on the macrodevice) you want to use" )
 print( "-- Press Control+C (on your normal keyboard) to exit\n" )
 
-function input_handler( event )
+function print_event( event )
 	
 	-- Only print key down events for libevdev
 	-- Remove if you want all events
@@ -43,3 +43,5 @@ function input_handler( event )
 	io.flush()
 	
 end
+
+macrodevice.open( backend, settings, print_event )
--- example.lua
+++ example.lua
@@ -1,17 +1,15 @@
 -- Example config for macrodevice-lua
 
+print( "macrodevice version "..macrodevice.version )
+
 -- The backend needs to be specified by setting the backend variable
-backend = "libusb"
+backend = "libevdev"
 
 -- The settings table specifies the device, and other settings.
--- The available setting keys depend on the backend, look at documentation/backends.md 
-settings = { vid = "046a", pid = "0011", use_bus_device = false, bus = 1, device = 18 }
+-- The available setting keys depend on the backend, look at documentation/backends.md
+settings = { eventfile = "/dev/input/by-id/u.sb-046a_0011-event-kbd", grab = true, numbers = false }
 
--- These settings are for the libevdev backend
--- settings = { eventfile = "/dev/input/by-id/u.sb-046a_0011-event-kbd", grab = true, numbers = false }
-
-
--- Whenever an input event occurs, the function input_handler gets called.
+-- Whenever an input event occurs, this function gets called.
 -- As an argument this function takes a table which describes the event.
 -- Important: this table always contains strings.
 function input_handler( event )
@@ -45,3 +43,30 @@ function input_handler( event )
 	end
 	
 end
+
+-- Open the device(s)
+-- multiple devices (with the same or different backend) can be opened simultaneously.
+macrodevice.open( backend, settings, input_handler )
+
+macrodevice.open(
+	"libusb",
+	{ vid = "046a", pid = "0011", use_bus_device = false, bus = 1, device = 18 },
+	function( event )
+		print( event[1].."\t"..event[2] )
+	end
+)
+
+
+-- After the devices have been opened, you can drop root privileges if required
+-- Option 1:
+macrodevice.drop_root( "username" )
+
+-- Option 2:
+user_id = 1000
+group_id = 1000
+if macrodevice.drop_root( user_id, group_id ) == 0 then
+	print( "success" )
+else
+	print( "failure" )
+end
+
